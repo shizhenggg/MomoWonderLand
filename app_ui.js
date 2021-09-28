dcfe057ed7c3ab83b897e7760729b40c8bcf0549
Shi Zheng <kangshizheng@hotmail.com>
2021-09-28 19:08:39 +0800

    github 404 link issue fix

--- app_ui.js
+++ app_ui.js
@@ -5,10 +5,10 @@ const volumeFunctions = () => {
         // function only works if audio file is defined
         if( $("audio").prop('muted')) {
                 $("audio").prop('muted', false)
-                $(".volumebtn").css("background-image","url(/assets/icon/volume.png)")} 
+                $(".volumebtn").css("background-image","url(assets/icon/volume.png)")} 
         else {
             $("audio").prop('muted', true)
-            $(".volumebtn").css("background-image","url(/assets/icon/mute.png)")
+            $(".volumebtn").css("background-image","url(assets/icon/mute.png)")
             }
         }
         )}
